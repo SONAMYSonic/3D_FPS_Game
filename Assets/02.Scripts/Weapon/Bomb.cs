6d546c2b29e37407955dfa274ae7c7ae17325792
AE86T <38212834+SONAMYSonic@users.noreply.github.com>
2025-12-12 12:44:18 +0900

    refactor: 리팩터링

--- Assets/02.Scripts/Weapon/Bomb.cs
+++ Assets/02.Scripts/Weapon/Bomb.cs
@@ -3,20 +3,24 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public GameObject ExplosionEffectPrefab;
+    [SerializeField] private float _bombDamage = 20f;
+
     private void OnCollisionEnter(Collision collision)
     {
         // Enemy에 닿으면 대미지 주고 폭발 이펙트 생성
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Monster monster = collision.gameObject.GetComponent<Monster>();
-            if (monster != null)
+            if (collision.gameObject.TryGetComponent<Monster>(out var monster))
             {
-                monster.TryTakeDamage(50f, gameObject.transform.position); // 예시로 50 대미지
+                monster.TryTakeDamage(_bombDamage, transform.position); // 예시로 50 대미지
                 Explode();
             }
         }
-
-        Explode();
+        else
+        {
+            Explode();
+        }
+        
     }
 
     private void Explode()
