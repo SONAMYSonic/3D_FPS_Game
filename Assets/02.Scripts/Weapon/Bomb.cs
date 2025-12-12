2b410a8ed10621538ddcb4ef0854961228d8fe20
AE86T <38212834+SONAMYSonic@users.noreply.github.com>
2025-12-12 12:37:49 +0900

    feat: 5.몬스터 폭탄 대미지

--- Assets/02.Scripts/Weapon/Bomb.cs
+++ Assets/02.Scripts/Weapon/Bomb.cs
@@ -5,6 +5,22 @@ public class Bomb : MonoBehaviour
     public GameObject ExplosionEffectPrefab;
     private void OnCollisionEnter(Collision collision)
     {
+        // Enemy에 닿으면 대미지 주고 폭발 이펙트 생성
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Monster monster = collision.gameObject.GetComponent<Monster>();
+            if (monster != null)
+            {
+                monster.TryTakeDamage(50f, gameObject.transform.position); // 예시로 50 대미지
+                Explode();
+            }
+        }
+
+        Explode();
+    }
+
+    private void Explode()
+    {
         GameObject effectObject = Instantiate(ExplosionEffectPrefab);
         effectObject.transform.position = transform.position;
 
